4221a05d374a7b7a642763fece2d9c4ef6b45e97
Anmolso107 <sonianmol285@gmail.com>
2025-12-30 09:54:03 +0530

    Files Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Google Form
+function redirectToSite1() {
+    window.location.href = "https://forms.gle/QAuaKSmkk1488aMMA";
+}
+
+// Schedule 
+function redirectToSite2() {
+    window.location.href = "https://drive.google.com/file/d/13K5XfukNPbC-svXIW1iL-ZtW1NnAJZ3N/view?usp=sharing";
+}
+
+// Event Details
+function redirectToSite3() {
+    window.location.href = "https://drive.google.com/file/d/1Ut-jcgzHe5C7SzO3r3iudRKCs-ua12x5/view?usp=sharing";
+}
+
+// 
+function redirectToSite4() {
+    window.location.href = "https://drive.google.com/file/d/1ytrJfGmrnnfQGhrsQHAPBfGXyv8mscJc/view?usp=sharing";
+}
